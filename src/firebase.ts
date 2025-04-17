2b2cee444d18e88ad71d7c8b5a5855de9551edc3
Mutlu Akgun <78053007+MutluRenegado@users.noreply.github.com>
2025-04-17 14:18:53 +0300

    Update and rename firebase.js to firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
-// src/firebase/firebase.js (or firebase.ts)
+// src/firebase/firebase.ts
+
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, onAuthStateChanged, signOut, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signOut, signInWithPopup, User } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 // Firebase configuration
@@ -15,14 +16,20 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and Firestore
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+// Google authentication provider
 const googleProvider = new GoogleAuthProvider();
 
 // Auth functions
 const loginWithGoogle = () => signInWithPopup(auth, googleProvider);
 const logout = () => signOut(auth);
-const listenForAuthChanges = (callback) => onAuthStateChanged(auth, callback);
 
-// Export
+// Listen for authentication state changes
+const listenForAuthChanges = (callback: (user: User | null) => void) => onAuthStateChanged(auth, callback);
+
+// Export the necessary objects and functions
 export { app, auth, db, googleProvider, loginWithGoogle, logout, listenForAuthChanges, setDoc, doc };
