5ef42db4d73c88496b154e41da7924396e1f9b9d
Mutlu Akgun <78053007+MutluRenegado@users.noreply.github.com>
2025-04-17 12:21:55 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { useState, useEffect } from 'react';
 import { loginWithGoogle, logout, listenForAuthChanges } from '../../firebase/auth';
@@ -118,7 +118,6 @@ export default function Home() {
     </main>
   );
 }
-'use client';
 
 import { useState, useEffect } from 'react';
 import { loginWithGoogle, logout, listenForAuthChanges } from '../../firebase/auth';
